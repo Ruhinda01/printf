1c2ae85823876b0496d6b09089af89ad475f4f20
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-25 13:34:56 -0700

    Try

--- printf.c
+++ printf.c
@@ -43,7 +43,8 @@ int process_fmt(const char *format, va_list list, fmt_s *fmt, int num)
 					count += fmt[index].fmt(list);
 				else
 				{
-					va_end(list);
+					count += _putchar('%');
+					count += _putchar(*format);
 					return (-1);
 				}
 			}
