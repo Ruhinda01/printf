5da8057e0c11b7920ff57e7b1a2335d2a45e6fbd
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-25 08:52:34 -0700

    Fix

--- char.c
+++ char.c
@@ -10,6 +10,14 @@ int handle_char(va_list args)
 	int count = 0;
 	int ch = va_arg(args, int);
 
-	count += _putchar(ch);
+	if (ch == '\0')
+	{
+		count += _putchar('\\');
+		count += _putchar('0');
+	}
+	else
+	{
+		count += _putchar(ch);
+	}
 	return (count);
 }
