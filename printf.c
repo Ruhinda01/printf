86c656948a8f3ef4a449bcb5c8b28a902ceecfbe
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-25 11:20:35 -0700

    Try

--- printf.c
+++ printf.c
@@ -1,44 +1,44 @@
 #include "main.h"
 /**
- * _printf - performs printf
- * @format: format specs
- *
+ * find_spec - finds spec
+ * @fmt: specifier
+ * @num: size of fmt[]
+ * @specifier: from theformat
+ * Return: -1
+ */
+int  find_spec(fmt_s *fmt, int num, char specifier)
+{
+	int i;
+
+	for (i = 0; i < num; i++)
+	{
+		if (specifier == fmt[i].specifier)
+			return (i);
+	}
+	return (-1);
+}
+/**
+ * process_fmt - process fmt
+ * @format: specifier
+ * @list: arguments list
+ * @fmt: specifiers and functions
+ * @num: size of fmt
  * Return: count
  */
-int _printf(const char *format, ...)
+int process_fmt(const char *format, va_list list, fmt_s *fmt, int num)
 {
-	va_list list;
-	int count = 0, num, i, found;
-	fmt_s fmt[] = {
-		{'c', handle_char},
-		{'s', handle_str},
-		{'%', handle_perc},
-	};
+	int count = 0, index;
 
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	while (*format != '\0')
+	while (*format)
 	{
 		if (*format == '%' && *(format + 1))
 		{
 			format++;
-			num = sizeof(fmt) / sizeof(fmt[0]);
-			found = 0;
-			for (i = 0; i < num; i++)
-			{
-				if (*format == fmt[i].specifier)
-				{
-					count += fmt[i].fmt(list);
-					found = 1;
-					break;
-				}
-			}
-			if (!found)
-			{
-				va_end(list);
+			index = find_spec(fmt, num, *format);
+			if (index >= 0)
+				count += fmt[index].fmt(list);
+			else
 				return (-1);
-			}
 		}
 		else if (*format == '%')
 		{
@@ -47,8 +47,32 @@ int _printf(const char *format, ...)
 		}
 		else
 			count += _putchar(*format);
-		format++;
 	}
+	return (count);
+}
+/**
+ * _printf - performs printf
+ * @format: format specs
+ *
+ * Return: count
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int count = 0;
+	fmt_s fmt[] = {
+		{'c', handle_char},
+		{'s', handle_str},
+		{'%', handle_perc},
+	};
+	int num = sizeof(fmt) / sizeof(fmt[0]);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+	count = process_fmt(format, list, fmt, num);
 	va_end(list);
+
 	return (count);
 }
